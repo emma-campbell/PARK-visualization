f607dd742365483f0144ebca0dfaaf66af7e6916
emma-campbell <ecampb10@u.rochester.edu>
2021-04-25 17:37:32 -0400

    added graph with styles

--- src/components/NewClient/index.js
+++ src/components/NewClient/index.js
@@ -4,12 +4,13 @@ import styled from "styled-components";
 // RECHART
 import {
   BarChart,
-  CartesianAxis,
+  CartesianGrid,
   XAxis,
   YAxis,
   Tooltip,
   Legend,
   Bar,
+  ResponsiveContainer
 } from "recharts";
 
 // LOGO
@@ -84,7 +85,50 @@ const Graph = styled.div`
 `;
 
 const NewClient = () => {
-  var data = [{ name: "Page A", uv: 400, pv: 2400, amt: 2400 }];
+  var data = [
+    {
+      name: "Page A",
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: "Page B",
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: "Page C",
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: "Page D",
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: "Page E",
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: "Page F",
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: "Page G",
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
 
   return (
     <NewClientDiv>
@@ -105,14 +149,17 @@ const NewClient = () => {
             <Face score="2" />
           </Scores>
           <Graph>
-            <BarChart width={730} height={250} data={data}>
-              <XAxis dataKey="name" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Bar dataKey="pv" fill="#003972" />
-              <Bar dataKey="uv" fill="#003972" />
-            </BarChart>
+            <ResponsiveContainer width="95%" height="95%">
+              <BarChart fontFamily={'Hind, sans-serif'} data={data}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip wrapperStyle={{fontFamily: 'Hind, san-serif'}}/>
+                <Legend wrapperStyle={{fontFamily: 'Hind, san-serif'}}/>
+                <Bar dataKey="pv" fill="#003972" />
+                <Bar dataKey="uv" fill="#003972" />
+              </BarChart>
+            </ResponsiveContainer>
           </Graph>
         </Data>
       </Content>
