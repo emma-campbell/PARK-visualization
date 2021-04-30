860c189ad3db6eedd8950975ffd9994e289be62a
emma-campbell <ecampb10@u.rochester.edu>
2021-04-30 08:39:18 -0400

    modal styles

--- src/components/ModalData/index.js
+++ src/components/ModalData/index.js
@@ -1,11 +1,18 @@
 import styled from "styled-components";
 
 const ModalDiv = styled.div`
-  margin-left: 25%;
-  padding-left: 100px;
+  border-radius: 9px;
+  margin-top: 15%;
+  margin-left: 15%;
+  padding-left: 6em;
+  padding-right: 6em;
+  padding-top: 2em;
+  padding-bottom: 2em;
   background-color: white;
   width: 50%;
+  height: 50%;
 `;
+
 const ModalData = () => {
   const categories = ["Jitter", "Shimmer", "Pitch"];
   return (
